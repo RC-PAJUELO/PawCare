4192dafa430502831d1799444c5f67844753fd20
RC-PAJUELO <pajuelosarmientorc@gmail.com>
2026-09-15 00:40:43 -0500

    Add files via upload

---
+++ PawCare.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE PawCare;
+GO
+
+USE PawCare;
+GO
+
+CREATE TABLE Mascotas
+(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    NombreMascota VARCHAR(100) NOT NULL,
+    NombreDueno VARCHAR(100) NOT NULL,
+    Tipo VARCHAR(20) NOT NULL,
+    Edad INT NOT NULL,
+    Telefono VARCHAR(9) NOT NULL,
+    Observaciones VARCHAR(500) NULL
+);
+GO
+
+ALTER TABLE Mascotas
+ADD CONSTRAINT CK_Mascotas_Tipo
+CHECK (Tipo IN ('Perro', 'Gato', 'Otro'));
+GO
+
+CREATE PROCEDURE spListarMascotas
+AS
+BEGIN
+    SELECT
+        Id,
+        NombreMascota,
+        NombreDueno,
+        Tipo,
+        Edad,
+        Telefono,
+        Observaciones
+    FROM Mascotas
+    ORDER BY Id DESC;
+END;
+GO
+
+CREATE PROCEDURE spInsertarMascota
+    @NombreMascota VARCHAR(100),
+    @NombreDueno VARCHAR(100),
+    @Tipo VARCHAR(20),
+    @Edad INT,
+    @Telefono VARCHAR(9),
+    @Observaciones VARCHAR(500)
+AS
+BEGIN
+    INSERT INTO Mascotas
+    (
+        NombreMascota,
+        NombreDueno,
+        Tipo,
+        Edad,
+        Telefono,
+        Observaciones
+    )
+    VALUES
+    (
+        @NombreMascota,
+        @NombreDueno,
+        @Tipo,
+        @Edad,
+        @Telefono,
+        @Observaciones
+    );
+END;
+GO
+
+
+SELECT * 
+FROM Mascotas
+ORDER BY Id DESC;
+
+SELECT COUNT(*) AS Cantidad
+FROM Mascotas;
